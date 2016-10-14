2b44f627056ceebadeda1ece599dd2d2f216db19
Gourav Tulsani <tulsanig1432@gmail.com>
2016-10-14 16:14:45 +0530

    mail sender

---
+++ mailscriipt.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+ 
+fromaddr = "tulsanig1432@gmail.com"
+toaddr = str(raw_input("enter sender's mailaddress"))
+ 
+msg = MIMEMultipart()
+ 
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = str(raw_input("Subject: "))
+
+body = str(raw_input("Body: "))
+ 
+msg.attach(MIMEText(body, 'plain'))
+ 
+filename = str(raw_input("NAME OF THE FILE WITH ITS EXTENSION: "))
+attachment = open(str(raw_input("PATH OF THE FILE: ")), "rb")
+ 
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+msg.attach(part)
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "wtctaukboifmdqul")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
