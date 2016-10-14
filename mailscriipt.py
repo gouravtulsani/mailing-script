fc5d2456fcdb5208f83299c79a72863b004922d6
Gourav Tulsani <tulsanig1432@gmail.com>
2016-10-15 02:24:52 +0530

    optional attachments

--- mailscriipt.py
+++ mailscriipt.py
@@ -17,17 +17,20 @@ msg['Subject'] = str(raw_input("Subject: "))
 body = str(raw_input("Body: "))
  
 msg.attach(MIMEText(body, 'plain'))
- 
-filename = str(raw_input("NAME OF THE FILE WITH ITS EXTENSION: "))
-attachment = open(str(raw_input("PATH OF THE FILE: ")), "rb")
- 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
- 
-msg.attach(part)
- 
+attachfile = str(raw_input("Attach a file (y/n)"))
+if attachfile == "y":
+
+	filename = str(raw_input("NAME OF THE FILE WITH ITS EXTENSION: "))
+	attachment = open(str(raw_input("PATH OF THE FILE: ")), "rb")
+
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload((attachment).read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+	msg.attach(part)
+
+
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(fromaddr, "xpgquodognpmeujt")
